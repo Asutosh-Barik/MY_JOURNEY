67b1b62973a1f0f7039167d3b00cb875097e2e4f
Asutosh-Barik <117289331+Asutosh-Barik@users.noreply.github.com>
2022-11-14 17:28:30 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,31 @@
+//QUESTION-Write a statement to find the largest of five numbers. Display an alert box to show the result.
+o=-5;
+p=-2;
+c=-6;
+d= 10;
+f=-1;
+if (o>p && o>c && o>d && o>f)
+{
+console.log(o)
+alert(o)
+}
+else if (p>o && p>c && p>d && p>f)
+{
+console.log(p)
+alert(p)
+}
+else if (c>o && c>p && c>d && c>f)
+{
+console.log(c)
+alert(c)
+}
+else if (d>o && d>c && d>p && d>f)
+{
+console.log(d)
+alert(d)
+}
+else
+{
+console.log(f)
+alert(f)
+}
